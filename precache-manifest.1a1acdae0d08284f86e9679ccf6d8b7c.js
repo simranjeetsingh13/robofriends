06478e5832ad0169087135bf9f23eec83bccbe63
simranjeetsingh13 <42966898+simranjeetsingh13@users.noreply.github.com>
2020-05-16 14:44:38 +0530

    Updates

---
+++ precache-manifest.1a1acdae0d08284f86e9679ccf6d8b7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd855524665f3d3b686e677f45262f6",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b58972df93ff7a0fa516",
+    "url": "/robofriends/static/css/main.e97677bd.chunk.css"
+  },
+  {
+    "revision": "ed9b6b7b3f5e1db31da5",
+    "url": "/robofriends/static/js/2.12e19136.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.12e19136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b58972df93ff7a0fa516",
+    "url": "/robofriends/static/js/main.4fd3d8d1.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
